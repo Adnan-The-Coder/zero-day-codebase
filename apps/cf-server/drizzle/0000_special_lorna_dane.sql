0dd69db6848090993afd6ca8f43fb9cd4e551a19
Adnan <adnanali23189@gmail.com>
2025-09-13 19:20:41 +0530

    drizzle migrations

--- apps/cf-server/drizzle/0000_special_lorna_dane.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `contactus` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL,
-	`email` text NOT NULL,
-	`subject` text NOT NULL,
-	`description` text NOT NULL,
-	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
